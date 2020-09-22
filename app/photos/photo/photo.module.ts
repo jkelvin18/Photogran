03db5013f03c6db2c64f0e737f3b3868556bb41c
James <jkelvin18@hotmail.com>
2020-09-21 22:21:40 -0300

    Iniciando repositório

---
+++ app/photos/photo/photo.module.ts
@@ -0,0 +1,16 @@
+import { PhotosComponent } from './../photo-list/photos/photos.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoComponent } from './photo.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [ PhotoComponent ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [ PhotoComponent ]
+})
+
+export class PhotoModule { }
